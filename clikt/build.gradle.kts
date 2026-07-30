4cf8195c2a3e5265fbcf8859e4222f78be406edd
Dan Brough <dan@danbrough.org>
2026-07-29 22:31:16 -0600

    added some basic clikt support to the demo

---
+++ clikt/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.konan.target.HostManager
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.publish)
+}
+
+kotlin {
+  applyDefaultHierarchyTemplate()
+  jvm()
+  linuxX64()
+  linuxArm64()
+
+  if (HostManager.hostIsMac) {
+    macosX64()
+    macosArm64()
+  }
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(projects.core)
+        api(libs.clikt)
+      }
+    }
+  }
+
+
+}
