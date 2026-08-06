accc19495c68341c6a65df738c7d5201a5deb3cf
Dan Brough <dan@danbrough.org>
2026-08-05 19:49:52 -0600

    update

--- demo/src/commonMain/kotlin/io/github/danbrough/katty/demoMain.kt
+++ demo/src/commonMain/kotlin/io/github/danbrough/katty/demoMain.kt
@@ -1,10 +1,6 @@
 package io.github.danbrough.katty
 
 
-import com.github.ajalt.mordant.rendering.TextAlign
-import com.github.ajalt.mordant.rendering.TextColors.Companion.rgb
-import com.github.ajalt.mordant.rendering.Theme
-import com.github.ajalt.mordant.table.grid
 import com.github.ajalt.mordant.terminal.Terminal
 import io.github.danbrough.katty.Bash.DateCommandHandler
 import io.github.danbrough.katty.Bash.PwdCommand
@@ -13,51 +9,8 @@ import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 
 
-/*
-class TestCommand() : CliktCommand("test") {
-
-  val message by option().defaultLazy { "Default message at ${Clock.System.now()}" }
-    .help("Message to print")
-  val count by option().int().default(1).help("How many times to print the message")
-
-  override fun run() {
-    for (n in 1..count) kTerminal.terminal.print(TextStyles.bold(TextColors.brightMagenta("test command. message: $message count: $n${if (n < count) SystemLineSeparator else ""}")))
-  }
-}
-
-
-class DateCommand() : CliktCommand("date") {
-  override fun run() {
-    val tz = TimeZone.currentSystemDefault()
-    val date = Clock.System.now().toLocalDateTime(tz)
-    kTerminal.terminal.print(TextStyles.bold(TextColors.brightCyan(("$commandName: $date $tz"))))
-  }
-}
-*/
-
-
-fun markdownTest() {
-  val t = Terminal()
-  t.println("Markdown test")
-  t.render(grid {
-    row("Grid Builder", "Supports", "Alignment")
-    row {
-      cell("Left") { align = TextAlign.LEFT }
-      cell("Center") { align = TextAlign.CENTER }
-      cell("Right") { align = TextAlign.RIGHT }
-    }
-  })
-
-  t.println("FINISHED")
-}
-
 fun demoMain(args: Array<String>) {
 
-  when (args.firstOrNull()) {
-    "markdown" -> markdownTest()
-    //"mordant" -> demoMordant()
-    else -> {}
-  }
 
   val configDir = Path(KattyUtils.getEnv("HOME")!!, ".katty")
 
@@ -72,7 +25,7 @@ fun demoMain(args: Array<String>) {
   }),Path(configDir, "history.txt"))*/
 
 
-  val terminal = KTerminal(Terminal(),Path(configDir, "history.txt"))
+  val terminal = KTerminal(Terminal(), Path(configDir, "history.txt"))
 
   terminal.commandHandlers.addAll(
     listOf(
@@ -80,6 +33,7 @@ fun demoMain(args: Array<String>) {
       DateCommandHandler,
       PwdCommand,
       DemoMordantCommand,
+      DemoMarkdownCommand,
       Bash.CdCommand,
       ConfigTestCommand,
       TomlTestCommand,
