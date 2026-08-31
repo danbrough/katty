fb60c2da27e193213364154422b45cb3376d244b
Dan Brough <dan@danbrough.org>
2026-08-31 16:02:51 -0600

    update

--- demo/src/commonMain/kotlin/io/github/danbrough/katty/TestCommandHandler.kt
+++ demo/src/commonMain/kotlin/io/github/danbrough/katty/TestCommandHandler.kt
@@ -1,6 +1,16 @@
 package io.github.danbrough.katty
 
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
+
 class TestCommandHandler(override val parent: CommandHandler) : CommandHandler {
+
+  private val prompt = "test# ".let {
+    it.length to (TextStyles.bold + TextColors.brightGreen)(it)
+  }
+
+  override suspend fun prompt(): Pair<Int, String> = prompt
+
   override suspend fun runCommand(
     kTerminal: KTerminal,
     cmdLine: String,
