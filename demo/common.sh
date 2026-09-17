0347bdebc6602ee2229ff5fa4126017f9e41f3ab
Dan Brough <dan@danbrough.org>
2026-09-17 15:31:56 -0600

    update

--- demo/common.sh
+++ demo/common.sh
@@ -2,6 +2,7 @@
 MODULE="${PWD##*/}"
 cd ..
 ROOT_DIR="$(pwd)"
+export GRADLE_USER_HOME="${GRADLE_USER_HOME="$HOME/.gradle"}"
 
 [ -z  "$KLOG_COLOR" ] && KLOG_COLOR=1
 [ -z "$KLOG_LEVEL" ] && KLOG_LEVEL=TRACE
