8769b9fa084d397e28490ffbccf30527f96178cd
Dan Brough <dan@danbrough.org>
2026-09-19 12:19:52 -0600

    update

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -6,12 +6,26 @@ import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.kmp.android.library)
   id("io.github.danbrough.katty.dokka")
   id("io.github.danbrough.katty.publishing")
 }
 
 kotlin {
   applyDefaultHierarchyTemplate()
+
+  android {
+    compileSdk { version = release(37) }
+    minSdk = 27
+    namespace = "io.github.danbrough.katty.utils"
+
+    packaging {
+      jniLibs {
+        useLegacyPackaging = true
+      }
+    }
+  }
+
   jvm {
     compilerOptions {
       jvmTarget = JvmTarget.JVM_17
@@ -20,6 +34,8 @@ kotlin {
 
   linuxX64()
   linuxArm64()
+  androidNativeArm64()
+  androidNativeX64()
 
   if (HostManager.hostIsMac) {
     macosX64()
@@ -41,6 +57,18 @@ kotlin {
         implementation(libs.kotlinx.io.core)
 
       }
+    }
+
+    val jvmSharedMain = create("jvmSharedMain") {
+      dependsOn(commonMain.get())
+    }
+
+    jvmMain{
+      dependsOn(jvmSharedMain)
+    }
+
+    androidMain{
+      dependsOn(jvmSharedMain)
     }
   }
 }
