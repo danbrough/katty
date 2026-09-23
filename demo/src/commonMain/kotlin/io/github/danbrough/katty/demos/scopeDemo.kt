ad50c099bb79b005b49cd4916659d24f482dac3f
Dan Brough <dan@danbrough.org>
2026-09-23 17:56:28 -0600

    update

--- demo/src/commonMain/kotlin/io/github/danbrough/katty/demos/scopeDemo.kt
+++ demo/src/commonMain/kotlin/io/github/danbrough/katty/demos/scopeDemo.kt
@@ -50,7 +50,7 @@ val scopeDemo = basicCommand("scopeDemo", "Demos how to manage scopes. args = [s
     "session" -> {
       log.info { "scopeDemo::session" }
       topScope {
-        this@basicCommand.runInternal()
+        log.info { "scopeDemo::topScope start" }
       }
     }
 
